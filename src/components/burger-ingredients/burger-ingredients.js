6aeb08296ced69001305d0789668e10b54f1c103
svetomir77 <svetomir7@yandex.ru>
2022-06-28 12:29:12 +0300

    added scrolling

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import burgerIngredients from "./burger-ingredients.module.css";
 import TabBar from './tab-bar/tab-bar';
 import IngredientList from './list/list';
 import dataTypes from '../../utils/data-types.js';
 
 function BurgerIngredients (props) {
+    const [currentTab, setCurrentTab] = useState('bun');
+    const onTabClick = (tab) => {
+        setCurrentTab(tab);
+        const el = document.getElementById(tab);
+        if (el) el.scrollIntoView({behavior: "smooth"});
+    }
     return (
         <div className={burgerIngredients.main}>
         <section>
-            <TabBar/>
+            <TabBar onTabClick={onTabClick} currentTab={currentTab}/>
         </section>
         <section className={`${burgerIngredients.scrollWrap} scroller`}>
             <section className='mt-10'>
-                <h2 className='text text_type_main-medium'>Булки</h2>
+                <h2 className='text text_type_main-medium' id='bun'>Булки</h2>
                 <IngredientList data={props.data.filter(item => item.type === 'bun')}></IngredientList>
             </section>
             <section className='mt-2'>
-                <h2 className='text text_type_main-medium'>Соусы</h2>
+                <h2 className='text text_type_main-medium' id='sauce'>Соусы</h2>
                 <IngredientList data={props.data.filter(item => item.type === 'sauce')}></IngredientList>
             </section>
             <section className='mt-2'>
-                <h2 className='text text_type_main-medium'>Начинки</h2>
+                <h2 className='text text_type_main-medium' id='main'>Начинки</h2>
                 <IngredientList data={props.data.filter(item => item.type === 'main')}></IngredientList>
             </section>
         </section>
--- src/components/burger-ingredients/tab-bar/tab-bar.js
+++ src/components/burger-ingredients/tab-bar/tab-bar.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
-const TabBar = props => {
-    const [current, setCurrent] = React.useState('bun');
+import PropTypes from 'prop-types';
+const TabBar = ({ currentTab, onTabClick }) => {
     const onClick = (current) => {
-        setCurrent(current);
+        onTabClick(current);
     }
     return (
         <div style={{ display: 'flex' }}>
-            <Tab value="bun" active={current === 'bun'} onClick={onClick}>
+            <Tab value="bun" active={currentTab === 'bun'} onClick={onClick}>
                 Булки
             </Tab>
-            <Tab value="sauce" active={current === 'sauce'} onClick={onClick}>
+            <Tab value="sauce" active={currentTab === 'sauce'} onClick={onClick}>
                 Соусы
             </Tab>
-            <Tab value="main" active={current === 'main'} onClick={onClick}>
+            <Tab value="main" active={currentTab === 'main'} onClick={onClick}>
                 Начинки
             </Tab>
         </div>
     )
+}
+
+TabBar.propTypes = {
+    currentTab: PropTypes.string.isRequired,
+    onTabClick: PropTypes.func.isRequired,
 }
 
 export default TabBar;
